dc4ef833b6d879a4f7d572ddf2200607232ba3dc
андрей <rocklee.101401@gmail.com>
2025-11-12 23:19:18 +0300

    Create massage.py

---
+++ massage.py
@@ -0,0 +1,16 @@
+service = "классический массаж" 
+price = 2653
+discount = 0.04
+sessions = 10
+total = price*sessions
+print ("услуга" , service)
+print ("стоимость" , price , "руб")
+print ("сеансы" ,sessions)
+
+if discount<=10:
+    discount_session = price*discount
+    final_total = sessions*discount_session
+
+print ("скидка 3%" , discount_session , "руб")
+print ("итоговая стоимость" , final_total , "руб")
+print ("БЛАГОДАРИМ ЗА ПОКУПКУ")
