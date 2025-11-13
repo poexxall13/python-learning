814dbf56c06bcf26958bbe2349a35a4d86c08a28
андрей <rocklee.101401@gmail.com>
2025-11-14 00:51:25 +0300

    Create basic.py

---
+++ basic.py
@@ -0,0 +1,8 @@
+def main():
+    print ("""Главня функция программы!""")
+    
+user_phone: str = input("ваш номер телефона?")
+print (user_phone)
+
+user_name = input("ваше имя?")
+print (user_name)
